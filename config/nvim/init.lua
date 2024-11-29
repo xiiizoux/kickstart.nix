afbdcd213198b4f7277da6d934f65f62e326ba05
xiiizoux <124485722+xiiizoux@users.noreply.github.com>
2024-11-29 19:25:51 +0800

    Update init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,14 +1,1 @@
--- 加载 packer 插件管理器
-vim.cmd [[packadd packer.nvim]]
-
--- 确保 packpath 设置正确，指向你的配置路径
-vim.opt.packpath = vim.fn.stdpath('config') .. '/pack'
-
--- 加载插件配置文件
-require('packers.packer')
-
--- 加载 floaterm 配置
-require('packers.floaterm')
-
 require("user")
-
