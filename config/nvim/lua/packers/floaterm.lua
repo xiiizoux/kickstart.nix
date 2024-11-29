3755f226b8f24c1d74390f04199a2d15e2a986ac
xiiizoux <124485722+xiiizoux@users.noreply.github.com>
2024-11-29 19:26:12 +0800

    Delete config/nvim/lua/packers directory

--- config/nvim/lua/packers/floaterm.lua
+++
@@ -1,9 +0,0 @@
--- 配置 floaterm 插件
-vim.g.floaterm_keymap_toggle = '<F12>'
-vim.g.floaterm_keymap_new = '<F11>'
-vim.g.floaterm_height = 0.8
-vim.g.floaterm_width = 0.8
-vim.g.floaterm_title = 'Terminal'
-
--- 确保返回空表，避免 'nil' 错误
-return {}
