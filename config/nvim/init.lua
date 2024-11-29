c993de8b82323b3e03eea90589ce2983a102eaaf
xiiizoux <124485722+xiiizoux@users.noreply.github.com>
2024-11-29 19:14:25 +0800

    Update init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,7 +1,10 @@
 require("user")
 
--- 初始化 packer
+-- 加载 packer 插件管理器
 vim.cmd [[packadd packer.nvim]]
 
--- 引用单独的插件配置文件
-require('plugins')
+-- 加载插件配置文件
+require('packers.packer')
+
+-- 加载 floaterm 配置
+require('packers.floaterm')
