f074eb49ed1718c8a830c66a0a41c8d4a6944ff6
xiiizoux <124485722+xiiizoux@users.noreply.github.com>
2024-11-29 19:15:25 +0800

    Create packer.lua

---
+++ config/nvim/lua/packers/packer.lua
@@ -0,0 +1,10 @@
+-- 使用 packer 管理插件
+return require('packer').startup(function(use)
+    -- Packer 自身
+    use 'wbthomason/packer.nvim'
+
+    -- floaterm 插件
+    use 'voldikss/vim-floaterm'
+
+    -- 在此处添加其他插件
+end)
