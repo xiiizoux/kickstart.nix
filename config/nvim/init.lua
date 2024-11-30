6194550ce8ea3a77ede1bc593df7786215077fc4
xiiizoux <124485722+xiiizoux@users.noreply.github.com>
2024-11-30 12:04:49 +0800

    Update init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,3 +1,3 @@
 require("user")
-let g:python_host_prog = '/opt/anaconda3/envs/nvim/bin/python'
-let g:python3_host_prog = '/opt/anaconda3/envs/nvim/bin/python'
+vim.g.python_host_prog = '/opt/anaconda3/envs/nvim/bin/python'
+vim.g.python3_host_prog = '/opt/anaconda3/envs/nvim/bin/python'
