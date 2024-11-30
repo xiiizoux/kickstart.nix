6d659ec59188451590714c54be99bb3b00c51f73
xiiizoux <124485722+xiiizoux@users.noreply.github.com>
2024-11-30 12:25:58 +0800

    Update wilder.lua

--- config/nvim/lua/plugins/wilder.lua
+++ config/nvim/lua/plugins/wilder.lua
@@ -1,33 +1,33 @@
 return {
 	{
 		"gelguy/wilder.nvim",
-		keys = { ":", "/", "?" }, -- 按键绑定
-		dependencies = { "catppuccin/nvim" }, -- 依赖插件
-		build = function()
-			vim.fn["UpdateRemotePlugins"]()
-		end, -- 更新 RemotePlugins
+		keys = {
+			":",
+			"/",
+			"?",
+		},
+		dependencies = {
+			"catppuccin/nvim",
+		},
+    build = function() vim.fn["UpdateRemotePlugins"]() end,
 		config = function()
 			local wilder = require("wilder")
-			local catppuccin = require("catppuccin.palettes")
-			local macchiato = catppuccin.get_palette("macchiato")
+			local macchiato = require("catppuccin.palettes").get_palette("macchiato")
 
-			-- 设置高亮（可选，若仍报错可以注释掉）
-			local text_highlight = wilder.make_hl("WilderText", {
-				{ a = 1 }, { a = 1 }, { foreground = macchiato.text },
-			})
-			local mauve_highlight = wilder.make_hl("WilderMauve", {
-				{ a = 1 }, { a = 1 }, { foreground = macchiato.mauve },
-			})
+			-- Create a highlight group for the popup menu
+			local text_highlight =
+				wilder.make_hl("WilderText", { { a = 1 }, { a = 1 }, { foreground = macchiato.text } })
+			local mauve_highlight =
+				wilder.make_hl("WilderMauve", { { a = 1 }, { a = 1 }, { foreground = macchiato.mauve } })
 
-			-- 启用 wilder.nvim
+			-- Enable wilder when pressing :, / or ?
 			wilder.setup({ modes = { ":", "/", "?" } })
 
-			-- 设置模糊过滤器和命令管道
+			-- Enable fuzzy matching for commands and buffers
 			wilder.set_option("pipeline", {
 				wilder.branch(
 					wilder.cmdline_pipeline({
 						fuzzy = 1,
-						fuzzy_filter = wilder.python_fuzzy_filter(),
 					}),
 					wilder.vim_search_pipeline({
 						fuzzy = 1,
@@ -35,9 +35,9 @@ return {
 				),
 			})
 
-			-- 渲染器配置
-			wilder.set_option("renderer", wilder.popupmenu_renderer(
-				wilder.popupmenu_border_theme({
+			wilder.set_option(
+				"renderer",
+				wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
 					highlighter = wilder.basic_highlighter(),
 					highlights = {
 						default = text_highlight,
@@ -51,8 +51,8 @@ return {
 					border = "rounded",
 					left = { " ", wilder.popupmenu_devicons() },
 					right = { " ", wilder.popupmenu_scrollbar() },
-				})
-			))
+				}))
+			)
 		end,
 	},
 }
