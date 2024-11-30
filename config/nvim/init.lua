14d9fcef1afba7118254b422a14053c31aac815a
xiiizoux <124485722+xiiizoux@users.noreply.github.com>
2024-11-30 16:48:27 +0800

    Update init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,4 +1,6 @@
 require("user")
+vim.o.mouse = ""  -- 禁用鼠标支持
+vim.o.mouse = "a"  -- 启用鼠标支持
 vim.api.nvim_set_option("clipboard", "unnamedplus")
 vim.g.python_host_prog = '/opt/anaconda3/envs/nvim/bin/python'
 vim.g.python3_host_prog = '/opt/anaconda3/envs/nvim/bin/python'
