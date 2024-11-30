64d1f225547ca19b8b6c122f6921029afc37b39e
xiiizoux <124485722+xiiizoux@users.noreply.github.com>
2024-11-30 15:55:32 +0800

    Update init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_set_option("clipboard", "unnamed")
+require("user")
+
 vim.g.python_host_prog = '/opt/anaconda3/envs/nvim/bin/python'
 vim.g.python3_host_prog = '/opt/anaconda3/envs/nvim/bin/python'
-require("user")
