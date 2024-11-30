74b3da127858dabb95480c3d60ade8b86b35bf9a
xiiizoux <124485722+xiiizoux@users.noreply.github.com>
2024-11-30 18:58:02 +0800

    Update init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -6,3 +6,10 @@ vim.api.nvim_set_option("clipboard", "unnamed")
 -- 通过conda设置python环境
 vim.g.python_host_prog = '/opt/anaconda3/envs/nvim/bin/python'
 vim.g.python3_host_prog = '/opt/anaconda3/envs/nvim/bin/python'
+
+-- 查询所有快捷键
+nnoremap("<leader>sk", function()
+	require("telescope.builtin").keymaps(require("telescope.themes").get_dropdown({
+		previewer = false,
+	}))
+end, { desc = "[S]earch [K]eymaps" })
