8b8d8c0bed641d0df692cd57ce7b030045749551
xiiizoux <124485722+xiiizoux@users.noreply.github.com>
2024-11-30 17:33:33 +0800

    Update init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,8 @@
 require("user")
 
+-- 设置clipboard
+vim.api.nvim_set_option("clipboard", "unnamed")
+
 -- 通过conda设置python环境
 vim.g.python_host_prog = '/opt/anaconda3/envs/nvim/bin/python'
 vim.g.python3_host_prog = '/opt/anaconda3/envs/nvim/bin/python'
