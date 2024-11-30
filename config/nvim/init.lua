12613797cdaf9b42a92054d8e72f7e936975fb97
xiiizoux <124485722+xiiizoux@users.noreply.github.com>
2024-11-30 17:07:06 +0800

    Update init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -5,34 +5,14 @@ vim.g.python_host_prog = '/opt/anaconda3/envs/nvim/bin/python'
 vim.g.python3_host_prog = '/opt/anaconda3/envs/nvim/bin/python'
 
 -- 设置远程剪切板功能
-function my_paste(reg)
-    return function(lines)
-
-        --[ 返回 “” 寄存器的内容，用来作为 p 操作符的粘贴物 ]
-        local content = vim.fn.getreg('"')
-        return vim.split(content, '\n')
-
-    end
-end
-
-if (os.getenv('SSH_TTY') == nil)
-then
-    --[ 当前环境为本地环境，也包括 wsl ]
-    opt.clipboard:append("unnamedplus")
-else
-    opt.clipboard:append("unnamedplus")
-    vim.g.clipboard = {
-      name = 'OSC 52',
-      copy = {
-        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-      },
-      paste = {
-        --[ 小括号里面的内容可能是毫无意义的，但是保持原样可能看起来更好一点 ]
-        ["+"] = my_paste("+"),
-        ["*"] = my_paste("*"),
-
-
+vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
     },
 }
-end
