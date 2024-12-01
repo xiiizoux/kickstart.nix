848f5a9a96c58e9aa4b6021dc5a22c115fed851b
xiiizoux <124485722+xiiizoux@users.noreply.github.com>
2024-12-01 21:41:43 +0800

    Update init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,13 @@
 require("user")
 
+-- 设置剪切板
+vim.api.nvim_set_option("clipboard", "unnamedplus")
+-- 从系统剪贴板复制/粘贴
+vim.keymap.set({ 'n', 'x' }, 'cp', '"+y')
+vim.keymap.set({ 'n', 'x' }, 'cv', '"+p')
+-- 在不更改寄存器的情况下删除
+vim.keymap.set({ 'n', 'x' }, 'x', '"_x')
+
 -- 通过conda设置python环境
 if vim.loop.os_uname().sysname == "Darwin" then
   -- macOS 环境
