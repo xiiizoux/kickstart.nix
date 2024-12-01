8f019bd805ec6301eaa33e28d5404df03f5b9780
xiiizoux <124485722+xiiizoux@users.noreply.github.com>
2024-12-01 23:59:59 +0800

    Update init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,13 +1,4 @@
--- 禁用鼠标功能
-vim.opt.mouse = ""
-
--- 设置剪切板
-vim.api.nvim_set_option("clipboard", "unnamed")
--- 从系统剪贴板复制/粘贴
-vim.keymap.set({ 'n', 'x' }, 'cp', '"+y')
-vim.keymap.set({ 'n', 'x' }, 'cv', '"+p')
--- 在不更改寄存器的情况下删除
-vim.keymap.set({ 'n', 'x' }, 'x', '"_x')
+require("user")
 
 -- 通过conda设置python环境
 if vim.loop.os_uname().sysname == "Darwin" then
@@ -19,5 +10,3 @@ else
   vim.g.python_host_prog = '/home/zou/anaconda3/envs/nvim/bin/python'
   vim.g.python3_host_prog = '/home/zou/anaconda3/envs/nvim/bin/python'
 end
-
-require("user")
