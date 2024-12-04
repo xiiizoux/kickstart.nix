a73045113448ba8329ddf4dce3babdb0de834ed0
xiiizoux <124485722+xiiizoux@users.noreply.github.com>
2024-12-04 09:54:46 +0800

    Update floaterm.lua

--- config/nvim/lua/packers/floaterm.lua
+++ config/nvim/lua/packers/floaterm.lua
@@ -4,7 +4,10 @@ return function(use)
   -- 使用 vim-floaterm 插件
   use 'voldikss/vim-floaterm'
 
-  -- 如果你想在此插件上添加一些配置，放在这里
-  -- 例如设置浮动终端的快捷键或启动方式
-  -- vim.g.floaterm_keymap_new = '<F12>'
+  -- 设置浮动终端的快捷键（可选）
+  vim.api.nvim_set_keymap('n', '<Leader>t', ':FloatermNew<CR>', { noremap = true, silent = true })
+  
+  -- 可选：其他配置
+  -- vim.g.floaterm_width = 0.9
+  -- vim.g.floaterm_height = 0.9
 end
