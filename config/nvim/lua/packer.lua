dbfad0e40782a1f80a0b9c715c72e081ad04e916
xiiizoux <124485722+xiiizoux@users.noreply.github.com>
2024-12-04 09:47:15 +0800

    Update packer.lua

--- config/nvim/lua/packer.lua
+++ config/nvim/lua/packer.lua
@@ -1,3 +1,5 @@
+-- ~/.config/nvim/lua/packer.lua
+
 local ensure_packer = function()
   local fn = vim.fn
   local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
@@ -11,14 +13,21 @@ end
 
 local packer_bootstrap = ensure_packer()
 
--- 插件配置
+-- 加载模块并初始化插件
 return require('packer').startup(function(use)
-  -- packer.nvim 本身
+  -- packer.nvim 自身
   use 'wbthomason/packer.nvim'
 
-  -- 示例插件
-  use 'voldikss/vim-floaterm'
-  -- use 'neovim/nvim-lspconfig'
+  -- 动态加载插件模块
+  local modules = { 'plugins.vim-floaterm' }
+  for _, mod in ipairs(modules) do
+    local ok, module = pcall(require, mod)
+    if ok then
+      module(use)
+    else
+      vim.notify('Error loading module: ' .. mod .. '\n' .. module, vim.log.levels.ERROR)
+    end
+  end
 
   -- 自动同步插件
   if packer_bootstrap then
