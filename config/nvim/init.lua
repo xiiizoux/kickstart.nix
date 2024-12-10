8121cc78600b05da4263c21d143189cc6522e09b
xiiizoux <124485722+xiiizoux@users.noreply.github.com>
2024-12-10 16:57:04 +0800

    Update init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,1 +1,12 @@
 require("user")
+
+-- 通过conda设置python环境
+if vim.loop.os_uname().sysname == "Darwin" then
+  -- macOS 环境
+  vim.g.python_host_prog = '/opt/anaconda3/envs/nvim/bin/python'
+  vim.g.python3_host_prog = '/opt/anaconda3/envs/nvim/bin/python'
+else
+  -- Ubuntu 环境
+  vim.g.python_host_prog = '/home/zou/anaconda3/envs/nvim/bin/python'
+  vim.g.python3_host_prog = '/home/zou/anaconda3/envs/nvim/bin/python'
+end
