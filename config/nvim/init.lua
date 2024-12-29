46a27c354e6ca5a8c56b0aa2465b965a52a35510
xiiizoux <124485722+xiiizoux@users.noreply.github.com>
2024-12-30 00:20:04 +0800

    Update init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -5,6 +5,10 @@ if vim.loop.os_uname().sysname == "Darwin" then
   -- macOS 环境
   vim.g.python_host_prog = '/opt/anaconda3/envs/nvim/bin/python'
   vim.g.python3_host_prog = '/opt/anaconda3/envs/nvim/bin/python'
+elseif vim.loop.os_uname().sysname == "Windows_NT" then
+  -- Windows 环境
+  vim.g.python_host_prog = 'C:\\Users\\ZOU\\anaconda3\\envs\\nvim\\python.exe'
+  vim.g.python3_host_prog = 'C:\\Users\\ZOU\\anaconda3\\envs\\nvim\\python.exe'
 else
   -- Ubuntu 环境
   vim.g.python_host_prog = '/home/zou/anaconda3/envs/nvim/bin/python'
